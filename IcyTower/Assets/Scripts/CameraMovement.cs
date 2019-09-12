d1fc8628e89f4c72df2c2a05c12a25f71d7fd39b
AntoinePlouffe <antoineplouffe0602@gmail.com>
2019-09-12 19:30:27 -0400

    Cours

---
+++ IcyTower/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Rigidbody m_Rigidbody;
+    public float m_Speed = 0f;
+    private float m_Delai = 0f;
+    Vector3 velocity = new Vector3();
+
+
+    private void Awake()
+    {
+
+    }
+
+    public void Update()
+    {
+         if (m_Delai < 1700)
+        {
+            transform.position += Vector3.up * m_Speed * Time.deltaTime;
+            m_Delai = m_Delai + 1;
+        }
+        else
+        {
+            m_Speed = 0f;
+        }
+    }
+
+
+}
