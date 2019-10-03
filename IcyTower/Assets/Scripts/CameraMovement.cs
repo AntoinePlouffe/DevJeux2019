4922441b6586fd229a0d2d33cb2701c046ca87c9
AntoinePlouffe <antoineplouffe0602@gmail.com>
2019-10-03 18:10:24 -0400

    Cours

--- IcyTower/Assets/Scripts/CameraMovement.cs
+++ IcyTower/Assets/Scripts/CameraMovement.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    public Vector3 m_Start;
+    public Vector3 m_End;
+
     public Rigidbody m_Rigidbody;
     public float m_Speed = 0f;
     private float m_Delai = 0f;
     Vector3 velocity = new Vector3();
 
+    private float m_Percentage;
 
     private void Awake()
     {
-
+        m_Percentage = 0f;
     }
 
     public void Update()
     {
+        m_Percentage += Time.deltaTime * 0.04f;
+        /*
          if (m_Delai < 1700)
         {
             transform.position += Vector3.up * m_Speed * Time.deltaTime;
@@ -26,6 +32,13 @@ public class CameraMovement : MonoBehaviour
         {
             m_Speed = 0f;
         }
+        */
+        if (m_Percentage >= 1f)
+        {
+            m_Percentage = 1f;
+        }
+
+        transform.position = Vector3.Lerp(m_Start, m_End, m_Percentage);
     }
 
 
